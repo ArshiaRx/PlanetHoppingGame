1e895a452a92005ed9326c79e9085b251e6f47c4
Pratik Patel <pratik.patel@ryerson.ca>
2022-03-26 17:59:27 -0400

    My code so far

--- Planet.java
+++ Planet.java
@@ -5,17 +5,16 @@ import java.util.*;
  */
 interface Planet {
     interface room{
+        public void readRoomFile()throws Exception;
         interface character{
-        
-        
+            public void readCharacterFile() throws Exception;
+            public void passiveMode() throws Exception;
         }
         interface items{
-    
-            
+            public void readItemFile();
         }
         interface monster{
-    
-            
+            public void readMonsterFile();
         }
     }
 }
