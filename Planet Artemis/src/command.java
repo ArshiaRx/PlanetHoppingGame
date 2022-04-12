a13cedcf208adbab0b61e6058ecea5d2993db74c
ArshiaRx <Arshiarahimrx@gmail.com>
2022-04-12 14:31:07 -0400

    Artemis Planet! Still working on it.

---
+++ Planet Artemis/src/command.java
@@ -0,0 +1,75 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class command {
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+
+    public static void main(String[] args) {
+        // Single letter commands (N, W, S, E, Q) should be vailable.
+
+        Scanner scanner = new Scanner(System.in);
+
+        locations.put(0, new Location(0, "you are heading west"));
+        locations.put(1, new Location(1, "you are heading south"));
+        locations.put(2, new Location(2, "you are in east direction"));
+        locations.put(5, new Location(5, "You are almost there to find the magical room"));
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 1);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+
+        Map<String, String> vocabulary = new HashMap<String, String>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+
+
+        int loc = 1;
+        while(true) {
+            System.out.println(locations.get(loc).getDescription());
+            if(loc == 0) {
+                break;
+            }
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("you can go to ... ");
+            for(String exit: exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word: words) {
+                    if(vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
+
+            } else {
+                System.out.println("You just been there");
+            }
+        }
+
+    }
+}
